92994b670e87487dfc8b292eeadaf8c8656b6857
devonpaluso <55293990+devonpaluso@users.noreply.github.com>
2024-01-10 11:06:24 -0500

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,4 @@
-import express, { Request, Response } from 'express';
 
-const PORT = 4000;
 
-const app = express();
-app.get('/*', (request: Request, response: Response) => {
-    response.send('ok');
-});
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`);
-});
+
+console.log('Hello World!')
